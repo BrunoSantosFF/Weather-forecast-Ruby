59bf4403217c3d3942aa7fab64666f8d7a7aef5e
Vinicius Amorim <viniamorim87@hotmail.com>
2024-09-20 21:31:52 -0300

    feat: adding css animations

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,13 +1,17 @@
+require 'date'
+
 class WeatherController < ApplicationController
   include HTTParty
   base_uri 'api.hgbrasil.com'
 
   def show
-    city = params[:city] 
-    api_key = '2ca29349' 
+    city = params[:city]
+    api_key = '757eaa52'
     response = self.class.get("/weather", query: { key: api_key, city_name: city })
     @weather = response.parsed_response['results']
+    today = Date.today
+    @date = today.strftime("%A, %B #{today.day.ordinalize}")
 
-    #render json: @weather
+    # render json: @weather
   end
 end
